6e2f2047968094820b9b18cb501691966787056c
박현서 <gustj960217@gmail.com>
2020-05-27 16:02:16 +0900

    feat: Add capture screen shot

---
+++ Assets/Demo/Scripts/Rotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    int _speed = 1;
+    [SerializeField]
+    UnityEngine.UI.Text _speedText;
+
+    void Update()
+    {
+        ChangeSpeed();
+
+        transform.Rotate(Vector3.up, 45*Time.deltaTime*_speed);
+    }
+
+    void ChangeSpeed()
+    {
+        bool up = Input.GetKeyDown(KeyCode.UpArrow);
+        bool down = Input.GetKeyDown(KeyCode.DownArrow);
+
+        if (up)
+        {
+            _speed += 1;
+            _speedText.text = _speed.ToString();
+        }
+
+        if (down)
+        {
+            _speed -= 1;
+            _speedText.text = _speed.ToString();
+        }
+    }
+}
---
+++ Assets/ScreenShotController.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class ScreenShotController : MonoBehaviour
+{
+    [SerializeField]
+    string folderName = "Default";
+    [SerializeField]
+    int howMany;
+    [SerializeField]
+    float timeScale = 0.3f;
+
+    bool running;
+    int frameCount;
+    string folderPath;
+    int index;
+
+    private void Update()
+    {
+        var space = Input.GetKeyDown(KeyCode.Space);
+
+        if (space)
+            Initialize();
+
+        if (running)
+        {
+            if (frameCount > howMany)
+            {
+                Debug.Log("Capture end");
+                running = false;
+                Time.timeScale = 1;
+            }
+
+            string filename = Path.Combine(folderPath, $"{index++}.png");
+            CaptureScreenshot.CaptureTransparentScreenshot(Camera.main, Screen.width, Screen.height, filename);
+            frameCount++;
+        }
+    }
+
+    public void Initialize()
+    {
+        if (running)
+        {
+            Debug.LogError("Capture is already running!");
+            return;
+        }
+
+        Debug.Log("Capture Start!!");
+
+        running = true;
+        
+        index = 0;
+        frameCount = 0;
+        Time.timeScale = timeScale;
+
+        folderPath = Path.Combine("Assets", folderName);
+        if (Directory.Exists(folderPath) == false)
+            Directory.CreateDirectory(folderPath);
+    }
+}
